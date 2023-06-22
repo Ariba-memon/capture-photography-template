c422b94b09fe5b9e613907a61e91a775c40b5bf9
Ariba-memon <aribaabdulqadir786@gmail.com>
2023-06-23 03:06:51 +0500

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,84 @@
+'use client'
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const [color, setColor] = useState('transparent')
+  const [textColor, setTextColor] = useState('white')
+  const handleNav = () => {
+    setNav(!nav)
+  }
+  // useEffect(() => {
+  //   const changeColor = () => {
+  //     if (window.scrollY >= 90) {
+  //       setColor('#ffffff')
+  //       setTextColor('#ffffff')
+  //     } else {
+  //       setColor('transparent')
+  //       setTextColor('#ffffff')
+  //     }
+  //   }
+  //   window.addEventListener('scroll', changeColor)
+  // }, [])
+  return (
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in-duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Link href="/">
+          <h1 style={{ color: `${textColor}` }} className="font-bold text-4xl">
+            Capture
+          </h1>
+        </Link>
+        <ul className="hidden sm:flex">
+          <li className="p-4">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/">Gallery</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/">Work</Link>
+          </li>
+          <li className="p-4">
+            <Link href="/">Contact</Link>
+          </li>
+        </ul>
+        {/* Mobile */}
+        <div onClick={handleNav} className="block sm:hidden z-10">
+          {nav ? (
+            <AiOutlineClose style={{ color: `${textColor}` }} size={20} />
+          ) : (
+            <AiOutlineMenu style={{ color: `${textColor}` }} size={20} />
+          )}
+        </div>
+        <div
+          className={
+            nav
+              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in-duration-300'
+              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in-duration-300'
+          }
+        >
+          <ul>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/">Gallery</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/">Work</Link>
+            </li>
+            <li className="p-4 text-4xl hover:text-gray-500">
+              <Link href="/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: [],
 }
